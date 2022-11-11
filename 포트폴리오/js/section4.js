cb1c67cf54e90afbd5f8410ea1968cd46787fa9f
hyeahyun2 <hgass66@naver.com>
2022-11-11 17:52:54 +0900

    update_section4.js

--- 포트폴리오/js/section4.js
+++ 포트폴리오/js/section4.js
@@ -1,3 +1,19 @@
 const sec4 = document.querySelector(".page4");
-const sec4_gory = sec4.querySelector(".Category_sec4");
+const sec4_gory = sec4.querySelectorAll(".Category_sec4 li");
 const sec4_post = sec4.querySelector(".Post_sec4");
+const sec4_h5 = sec4_post.querySelector("h5");
+const sec4_list = sec4_post.querySelectorAll("ul");
+console.log(sec4_gory);
+sec4_gory.forEach((element, i) => {
+  element.addEventListener("click",(q)=>{
+    sec4_h5.innerText = sec4_gory[i].innerText;
+    sec4_gory.forEach(not =>{
+      not.classList.remove("check_gory");
+    })
+    q.currentTarget.classList.add("check_gory");
+    sec4_list.forEach(e =>{
+      e.classList.remove("check_sec4");
+    })
+    sec4_list[i].classList.add("check_sec4");
+  })
+});
