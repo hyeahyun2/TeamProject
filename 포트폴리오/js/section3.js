ef718b1b84566673c1b01b5ecf4b1c6707703fc4
JJoon <chipp0604@gmail.com>
2022-11-13 17:09:01 +0900

    해준 js 수정

--- 포트폴리오/js/section3.js
+++ 포트폴리오/js/section3.js
@@ -3,7 +3,7 @@
 // const lis = sList.querySelectorAll('li');
 // const btn = document.querySelectorAll('#btnWrap_hj button');
 // const lisCopy1 = lis[0].cloneNode(true);
-// const lisCopy2 = lis[lis.length-1].cloneNode(true)
+// const lisCopy2 = lis[lis.length-1].cloneNode(true);
 // let lisNum = lis.length + 2;
 // console.log(lisNum);
 // let sNum = 10;
@@ -18,6 +18,7 @@
 //     sNum += 10;
 //     sList.style.transition = '1s';
 //     sList.style.transform = `translateX(-${sNum}%)`;
+    
 //     setTimeout(()=>{
 //       if ( sNum == 50 ) {
 //         sList.style.transition = '0s';
@@ -61,66 +62,115 @@
 // })
 
 
-const slides = document.querySelector('.Slider_hj');
-const slide = document.querySelectorAll('.Slider_hj li');
-const currentIdx = 0;
-const slideCount = slide.length;
-const slideWidth = 25;
-prevBtn = document.querySelector('.prev_hj');
-nextBtn = document.querySelector('.next_hj');
+// const slides_sec3 = document.querySelector('.Slider_hj');
+// const slide_sec3 = document.querySelectorAll('.Slider_hj li');
+// const currentIdx_sec3 = 0;
+// const slideCount_sec3 = slide.length;
+// const slideWidth_sec3 = 25;
+// prevBtn_sec3 = document.querySelector('.prev_hj');
+// nextBtn_sec3 = document.querySelector('.next_hj');
 
-makeClone();
+// makeClone_sec3();
 
-function makeClone() { 
-  for (let i = 0; i<slideCount; i++) {
-      // a.cloneNode(), a.cloneNode(true)
-      let cloneSlide = slide[i].cloneNode(true);
-      cloneSlide.classList.add('clone');
-      //a.appendChild(b)
-      slides.appendChild(cloneSlide);
+// function makeClone_sec3() { 
+//   for (let i = 0; i<slideCount_sec3; i++) {
+//       // a.cloneNode(), a.cloneNode(true)
+//       let cloneSlide_sec3 = slide[i].cloneNode(true);
+//       cloneSlide_sec3.classList.add('clone');
+//       //a.appendChild(b)
+//       slides_sec3.appendChild(cloneSlide);
+//   }
+//   for (let i = slideCount_sec3 - 1; i>=0; i--) {
+//       let cloneSlide_sec3 = slide[i].cloneNode(true);
+//       cloneSlide_sec3.classList.add('clone');
+//       //a.prepend(b)
+//       slides_sec3.prepend(cloneSlide_sec3);
+//   }
+//   // ul의 넓이를 지정하기 위해 새로운 함수 호출
+//   updateWidth_sec3();
+//   // 초기 위치 함수
+//   setInitialPos_sec3(); 
+
+//   setTimeout_sec3(function (){
+//     slides.classList.add('animated');
+//   },100);
+// }
+
+// function updateWidth_sec3() { 
+//   // 새로 복사된 li들 까지 모두 부른 후 그 길이를 
+//   // newSlideCount 변수에 넣는다.
+//   let currentSlides_sec3 = document.querySelectorAll('.Slider_hj li');
+//   let newSlideCount_sec3 = currentSlides_sec3.length;
+//   // 그다음의 슬라이더 갯수만큼 넓이를 구한뒤 ul 넓이 지정
+//   let newWidth_sec3 = slideWidth_sec3 * newSlideCount_sec3 + `%`;
+//   slides_sec3.style.width = newWidth_sec3;
+// }
+
+// function setInitialPos_sec3() {
+//   let initialTranslateValue_sec3 = -(slideWidth_sec3 * slideCount_sec3);
+//   // slides ( transform: translateX(-값%))
+//   slides_sec3.style.transform = `translateX(-33.28%)`;
+//   console.log(slides_sec3.style.transform);
+// }
+
+// nextBtn_sec3.addEventListener('click', function(){
+//   moveSlide(currentIdx_sec3 + 1); 
+// });
+// nextBtn_sec3.addEventListener('click', function(){
+//   moveSlide(currentIdx_sec3 - 1); 
+// });
+
+// function moveSlide_sec3(num){
+//   slides_sec3.style.left = -num * slideWidth_sec3 + '%';
+//   currentIdx_sec3 = num;
+// }
+
+const sWrap = document.getElementById('SliderWrap_hj');
+const sList = document.querySelector('.Slider_hj');
+const list = sList.querySelectorAll('li');
+const btn = document.querySelectorAll('#btnWrap_hj button');
+
+let sNum = 0;
+let state = 1;
+
+let nextSlider = ()=> {
+    sNum += 25;
+    sList.style.transition = '1s';
+    sList.style.transform = `translateX(-${sNum}%)`;
+    sList.appendChild(list[0]);
+   
+}
+let prevSlider = ()=> {
+    sNum -= 10;
+    sList.style.transition = '1s';
+    sList.style.transform = `translateX(-${sNum}%)`;
+    setTimeout(()=>{
+      if ( sNum == 0 && state == 0 ) {
+        sList.style.transition = '0s';
+        sList.style.transform = `translateX(-${60}%)`;
+        sNum = 60;
+      }
+      state = 1;
+    }, 1000)
+}
+
+// let timer = setInterval(nextSlider, 3000);
+
+btn[1].addEventListener('click', ()=> {
+  clearInterval(timer);
+  if ( state == 1) {
+    state = 0;
+    nextSlider();
   }
-  for (let i = slideCount - 1; i>=0; i--) {
-      let cloneSlide = slide[i].cloneNode(true);
-      cloneSlide.classList.add('clone');
-      //a.prepend(b)
-      slides.prepend(cloneSlide);
+})
+btn[0].addEventListener('click', ()=> {
+  clearInterval(timer)
+  if ( state == 1 ) {
+    state = 0;
+    prevSlider();
   }
-  // ul의 넓이를 지정하기 위해 새로운 함수 호출
-  updateWidth();
-  // 초기 위치 함수
-  setInitialPos(); 
+})
 
-  setTimeout(function (){
-    slides.classList.add('animated');
-  },100);
-}
 
-function updateWidth() { 
-  // 새로 복사된 li들 까지 모두 부른 후 그 길이를 
-  // newSlideCount 변수에 넣는다.
-  let currentSlides = document.querySelectorAll('.Slider_hj li');
-  let newSlideCount = currentSlides.length;
-  // 그다음의 슬라이더 갯수만큼 넓이를 구한뒤 ul 넓이 지정
-  let newWidth = slideWidth * newSlideCount + `%`;
-  slides.style.width = newWidth;
-}
 
-function setInitialPos() {
-  let initialTranslateValue = -(slideWidth * slideCount);
-  // slides ( transform: translateX(-값%))
-  slides.style.transform = `translateX(-200%)`;
-  console.log(slides.style.transform);
-}
-
-nextBtn.addEventListener('click', function(){
-  moveSlide(currentIdx + 1); 
-});
-nextBtn.addEventListener('click', function(){
-  moveSlide(currentIdx - 1); 
-});
-
-function moveSlide(num){
-  slides.style.left = -num * slideWidth + '%';
-  currentIdx = num;
-}
 
