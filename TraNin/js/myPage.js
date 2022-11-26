e620ad9c1ea54d85ba039ed5fd74254c70f69d0d
hd03081 <82138596+hd03081@users.noreply.github.com>
2022-11-26 23:36:50 +0900

    1126 mypage2

--- TraNin/js/myPage.js
+++ TraNin/js/myPage.js
@@ -1,5 +1,8 @@
 const myPageProfileNickName = document.querySelector("#myPageProfileNickName");
 const myPageProfileIntroduce = document.querySelector("#myPageProfileIntroduce");
+const myPageProfilePicture = document.querySelector("#myPageProfilePicture");
+const myPageProfileAttach = document.querySelector("#myPageProfileAttach");
+
 myPageProfileNickName.addEventListener('focusout',function(){
   //ajax myPageProfileNickName 바꾸기
   console.log("ajax focusout1");
@@ -7,4 +10,8 @@ myPageProfileNickName.addEventListener('focusout',function(){
 myPageProfileIntroduce.addEventListener('focusout',function(){
   //ajax myPageProfileIntroduce 바꾸기
   console.log("ajax focusout2");
+});
+myPageProfileAttach.addEventListener('change',function(){
+  //ajax 프로필 사진 change시 서버에 이미지 파일 추가 및 데이터베이스url추가
+  console.log("ajax change picture");
 });
