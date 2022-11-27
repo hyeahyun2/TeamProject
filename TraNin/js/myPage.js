5e9bfbe0efc77ced146d7223656f82fc337ba9a0
hd03081 <82138596+hd03081@users.noreply.github.com>
2022-11-27 17:03:24 +0900

    mypage1127

--- TraNin/js/myPage.js
+++ TraNin/js/myPage.js
@@ -2,6 +2,10 @@ const myPageProfileNickName = document.querySelector("#myPageProfileNickName");
 const myPageProfileIntroduce = document.querySelector("#myPageProfileIntroduce");
 const myPageProfilePicture = document.querySelector("#myPageProfilePicture");
 const myPageProfileAttach = document.querySelector("#myPageProfileAttach");
+const myPageMyActivityLeftNav = document.querySelector("#myPageMyActivityLeftNav");
+const myPageMyActivityLeftNavLi = document.querySelectorAll("#myPageMyActivityLeftNav li");
+const myPageTab = document.querySelectorAll("#myPageTab li");
+const myPageFavoriteUl = document.querySelector("#myPageFavorite ul");
 
 myPageProfileNickName.addEventListener('focusout',function(){
   //ajax myPageProfileNickName 바꾸기
@@ -14,4 +18,29 @@ myPageProfileIntroduce.addEventListener('focusout',function(){
 myPageProfileAttach.addEventListener('change',function(){
   //ajax 프로필 사진 change시 서버에 이미지 파일 추가 및 데이터베이스url추가
   console.log("ajax change picture");
+});
+for(let i=0;i<myPageTab.length;i++){
+  console.log(i);
+  myPageTab[i].addEventListener('click',function(){
+    for(let j=0;j<myPageTab.length;j++){
+      myPageTab[j].classList.remove("active");
+    }
+    myPageTab[i].classList.add("active");
+  });
+}
+for(let i=0;i<myPageMyActivityLeftNavLi.length;i++){
+  console.log(i);
+  myPageMyActivityLeftNavLi[i].addEventListener('click',function(){
+    for(let j=0;j<myPageMyActivityLeftNavLi.length;j++){
+      myPageMyActivityLeftNavLi[j].classList.remove("enabled");
+    }
+    myPageMyActivityLeftNavLi[i].classList.add("enabled");
+  });
+}
+myPageFavoriteUl.addEventListener('scroll',function(){
+  if( myPageFavoriteUl.scrollTop === (myPageFavoriteUl.scrollHeight - myPageFavoriteUl.offsetHeight)){
+    console.log(myPageFavoriteUl.scrollTop+"ajax 발동");
+    let li = document.createElement("li")
+    myPageFavoriteUl.append(li);
+  }
 });
